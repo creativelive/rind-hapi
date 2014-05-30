20e496c621f2920346debb078b376eddd3a5d3d0
diffsky <matthew.hadley@gmail.com>
2014-05-30 13:34:38 -0700

    use abner for test

--- test/index.js
+++ test/index.js
@@ -1,18 +1,19 @@
 'use strict';
 
-var server = require('./server');
+var server = require('abner')();
 var test = require('tape');
 var Chaps = require('chaps');
 var chaps = new Chaps({
   debug: true,
-  hostname: 'localhost',
+  hostname: 'localhost:',
   timeout: 2000
 });
+var rind = require('..');
 
 test('plugin should add context object to views', function(t) {
   t.plan(1);
 
-  server.pack.register(require('..'), {
+  server.pack.register(rind, {
     conf: {
       foo: 'foo'
     }
@@ -22,11 +23,23 @@ test('plugin should add context object to views', function(t) {
     }
   });
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      handler: function(request, reply) {
+        reply.view('main', {
+          dump: 'bar'
+        });
+      }
+    }
+  });
+
   // start the server
   server.start(function() {
     console.log('server started:', server.info.uri);
     chaps.get({
-      url: ':9615/'
+      url: server.info.port
     }, function(err, data) {
       if (err) {
         t.fail('failed to talk to test server');
@@ -34,6 +47,7 @@ test('plugin should add context object to views', function(t) {
       server.stop(function() {
         console.log('server stopped');
       });
+      console.log('output:', data.text);
       t.equal(data.text, 'true foo bar\n');
     });
   });
